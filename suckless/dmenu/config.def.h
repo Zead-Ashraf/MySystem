9663cbf7e4e0bb18736dc2e6c5919fd2b585c226
Zead-Ashraf <zezohttp@gmail.com>
2023-07-20 21:11:23 +0300

    patch dmenu-fuzzyhighlight-4.9.diff

--- suckless/dmenu/config.def.h
+++ suckless/dmenu/config.def.h
@@ -11,6 +11,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -23,4 +25,4 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 2;
