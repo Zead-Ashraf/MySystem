e20f46d67242191946cde5b3eecd0026f5dcddb1
Zead-Ashraf <zezohttp@gmail.com>
2023-08-27 19:59:15 +0300

    add taskwarrior habits

--- scripts/add_taskwarrior_habits.sh
+++ scripts/add_taskwarrior_habits.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # add tasks
-task add priority:H "Study CCNA course" project:"CCNA" +Studying +Learning
-task add priority:H "Translate words (Charlote's Web)" project:"English" +Studying
-task add priority:H "Study Linux+ course" project:"Linux+" +Studying +Learning
-task add priority:H "Study VIM" project:"VIM" +Studying +Learning
+task add priority:H "Study CCNA course" project:"CCNA" +Studying +Learning +Habit
+task add priority:H "Translate words (Charlote's Web)" project:"English" +Studying +Habit
+task add priority:H "Study Linux+ course" project:"Linux+" +Studying +Learning +Habit
+task add priority:H "Study VIM" project:"VIM" +Studying +Learning +Habit
 
